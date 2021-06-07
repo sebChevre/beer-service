94588c9c24bc1a3400234844f91bfa4944120660
Sébastien Chèvre <sebastien.chevre@softcom.pro>
2021-06-07 21:47:20 +0200

    add base path

--- Startup.cs
+++ Startup.cs
@@ -106,7 +106,7 @@ namespace BeerApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UsePathBase("/beer-service");
+            //app.UsePathBase("/beer-service");
             
 
             ConfigureSwagger(app);
